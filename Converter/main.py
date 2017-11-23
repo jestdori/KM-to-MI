15c0d6ba93bdeb3eca381ab1f91325a7e854dae0
Matej Vidmar <jestdori@gmail.com>
2017-11-23 14:44:27 +0100

    popravek zanke

--- Converter/main.py
+++ Converter/main.py
@@ -1,25 +1,15 @@
 # Pozdrav Uporabniku
 print "Hello my name is KiloMile and i convert Kilometers to Miles. Enter value below and watch the magic happen :)"
 
-# Factor iz KM V Miles
-conv_fac = 0.621372
+print "Do you want to do a conversion? Answer yes or no. "
 
-# Vnos Kilometrou
-kilometers = int(raw_input("Enter value in kilometers:" ) )
-
-# Izracun In Prikaz
-miles = kilometers * conv_fac
-print('%0.4f kilometers is equal to %0.4f miles' %(kilometers,miles))
-
-# Vprasanje o Nadalnji uporabi
-print "Do you want to do a new conversion? Answer yes or no. "
 answer = (raw_input())
 
-# Loop
 while answer == "yes":
     # Vnos Kilometrou
     kilometers = int(raw_input("Enter value in kilometers:"))
     # Izracun in Prikaz
+    conv_fac = 1.609
     miles = kilometers * conv_fac
     print('%0.4f kilometers is equal to %0.4f miles' % (kilometers, miles))
     # Vprasanje o Nadalnji uporabi
